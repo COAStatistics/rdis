26b5dd8dcdcb59b677883e40be0a58689dd00ed0
timy116 <timy117@gmail.com>
2018-09-07 11:13:07 +0800

    Add output_excel.py
    output_excel:
    init var

---
+++ rdis/output_excel.py
@@ -0,0 +1,15 @@
+import json
+import openpyxl
+
+SAMPLE_PATH = '..\\..\\input\\simple_sample.txt'
+JOSN_PATH = '..\\..\\output\\json\\公務資料.json'
+
+# sorted by county
+sample_dict = {}
+official_data = {}
+
+def read_official_data():
+    pass
+
+def read_sample():
+    pass
